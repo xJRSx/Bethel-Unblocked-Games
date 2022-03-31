01477356a467fa896ea18e267af94c1d1808cabc
abcoding5 <abcoding5@gmail.com>
2022-03-31 00:44:37 +0000

    setup

--- timer.js
+++ timer.js
@@ -2,7 +2,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 30, 2022 1:21:00").getTime();
+var countDownDate = new Date("Mar 31, 2022 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -32,11 +32,7 @@ var x = setInterval(function() {
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "Timer Expired";
-    document.getElementById("updateDate").outerHTML = "<p1>Update 3/31</p1>";
-    document.getElementById("newGame").outerHTML = "<p2>New Game : </p2><p2><a href='/indexes/tanukiSunset.html'>Tanuki Sunset</a></p2>";
+    
 
-  } else {
-    document.getElementById("newGame").outerHTML = "<p2>New Game : </p2><p2><a href='/indexes/sansFight.html'>Sans Fight</a></p2>";
-    document.getElementById("updateDate").outerHTML = "<p1>Update 3/30</p1>";
   }
 }, 1000);
